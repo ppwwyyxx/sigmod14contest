e852b29d85aef5ed409691aa0109bf539334e4a7
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-02-28 21:34:53 +0800

    fast read

--- src/read.cpp
+++ src/read.cpp
@@ -1,5 +1,5 @@
 //File: read.cpp
-//Date: Fri Feb 28 15:29:17 2014 +0800
+//Date: Fri Feb 28 21:34:37 2014 +0800
 //Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 #include "lib/debugutils.h"
@@ -13,36 +13,85 @@
 using namespace std;
 using namespace std::tr1;
 
+const int BUFFER_LEN = 1024 * 1024 * 5;
+
+namespace {
+	char buffer[BUFFER_LEN];
+	char tmpBuf[1024];
+	char *ptr, *buf_end;
+}
+
 inline FILE* safe_open(const string& fname) {
 	FILE* fin = fopen(fname.c_str(), "r");
+	ptr = buffer, buf_end = ptr + 1;
 	m_assert(fin != NULL);
 	return fin;
 }
 
-#define MAX_LINE_LEN 65536
-namespace {
-	char buf[MAX_LINE_LEN];
-}
+#define PTR_NEXT() \
+	{ \
+		ptr ++; \
+		if (ptr == buf_end) \
+		{ \
+			ptr = buffer; \
+			buf_end = buffer + fread(buffer, 1, BUFFER_LEN, fin); \
+		} \
+	}
+#define READ_INT(_x_) \
+	{ \
+		while ((*ptr < '0' || *ptr > '9') && *ptr != '-') \
+			PTR_NEXT(); \
+		int register _n_ = 0; \
+		while (*ptr >= '0' && *ptr <= '9') \
+		{ \
+			_n_ = _n_ * 10 + *ptr - '0'; \
+			PTR_NEXT(); \
+		} \
+		(_x_) = (_n_); \
+	}
+#define READ_STR(_s_) \
+	{ \
+		char *_p_ = (_s_); \
+		while (*ptr != '|') \
+		{ \
+			*(_p_ ++) = *ptr; \
+			PTR_NEXT(); \
+		} \
+		*_p_ = 0; \
+	}
+
+#define READ_TILL_EOL() \
+	{ \
+		while ((*ptr != '\n') and (buf_end != buffer)) PTR_NEXT(); \
+	}
 
 void read_person_file(const string& dir) {
 	FILE* fin = safe_open(dir + "/person.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	READ_TILL_EOL();
 	int pid, maxid = 0;
-	while (fscanf(fin, "%d|", &pid) == 1) {
+	while (true) {
+		READ_INT(pid);
+		if (buf_end == buffer) break;
 		update_max(maxid, pid);
-		fgets(buf, MAX_LINE_LEN, fin);
+		READ_TILL_EOL();
 	}
 	P("Max id of Nodes: "); P(maxid);P("\n");
 	Data::allocate(maxid);
 
 	// read birthday
 	rewind(fin);
-	fgets(buf, MAX_LINE_LEN, fin);
+	ptr = buffer, buf_end = ptr + 1;
+	READ_TILL_EOL();
 	int year, month, day;
-	while (fscanf(fin, "%d|", &pid) == 1) {
-		for (int i = 0; i < 3; i ++) while (fgetc(fin) != '|') {}
-		fscanf(fin, "%d-%d-%d", &year, &month, &day);
-		fgets(buf, MAX_LINE_LEN, fin);
+	while (true) {
+		READ_INT(pid);
+		if (buffer == buf_end) break;
+		PTR_NEXT();
+		READ_STR(tmpBuf); READ_STR(tmpBuf); READ_STR(tmpBuf);
+		READ_INT(year);PTR_NEXT();
+		READ_INT(month);PTR_NEXT();
+		READ_INT(day);
+		READ_TILL_EOL();
 		Data::birthday[pid] = year * 10000 + month * 100 + day;
 	}
 	fclose(fin);
@@ -50,9 +99,13 @@ void read_person_file(const string& dir) {
 
 void read_person_knows_person(const string& dir) {
 	FILE* fin = safe_open(dir + "/person_knows_person.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	READ_TILL_EOL();
 	int p1, p2;
-	while (fscanf(fin, "%d|%d", &p1, &p2) == 2) {
+	while (true) {
+		READ_INT(p1);
+		if (buffer == buf_end) break;
+		READ_INT(p2);
+		PTR_NEXT();
 		Data::pp_map[p1][p2] = Data::pp_map[p2][p1] = true;
 	}
 	fclose(fin);
@@ -62,11 +115,14 @@ void read_comments(const string& dir) {
 	Timer timer;
 	print_debug("%lf\n", timer.get_time());
 	FILE* fin = safe_open(dir + "/comment_hasCreator_person.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	READ_TILL_EOL();
 	unsigned cid, pid;
 	vector<int> owner;
 	owner.reserve(4000000);
-	while (fscanf(fin, "%d|%d", &cid, &pid) == 2) {
+	while (true) {
+		READ_INT(cid);
+		if (buffer == buf_end) break;
+		READ_INT(pid);
 		m_assert(cid % 10 == 0);
 		m_assert(cid / 10 == owner.size());
 		owner.push_back(pid);
@@ -76,20 +132,22 @@ void read_comments(const string& dir) {
 	print_debug("After Read comment creator: %lf seconds\n", timer.get_time());
 	// read comment->comment
 	fin = safe_open(dir + "/comment_replyOf_comment.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	READ_TILL_EOL();
 
 	int ** comment_map = new int*[Data::nperson];
 	for (int i = 0; i < Data::nperson; i ++)
 		comment_map[i] = new int[Data::nperson]();
 
 	int cid1, cid2;
-	while (fscanf(fin, "%d|%d", &cid1, &cid2) == 2) {
+	while (true) {
+		READ_INT(cid1);
+		if (buffer == buf_end) break;
+		READ_INT(cid2);
+
 		int p1 = owner[cid1 / 10], p2 = owner[cid2 / 10];
 		if (not Data::pp_map[p1][p2]) continue;
-		if (p1 < p2)
-			comment_map[p1][p2] += 1;
-		else
-			comment_map[p2][p1] += 1;
+		if (p1 < p2) comment_map[p1][p2] += 1;
+		else comment_map[p2][p1] += 1;
 	}
 	fclose(fin);
 	print_debug("After Read comment replyOf: %lf seconds\n", timer.get_time());
@@ -111,19 +169,19 @@ void read_tags_forums(const string & dir) {
 	int tid, pid, fid;
 	{		// read tag and tag names
 		FILE* fin = safe_open(dir + "/tag.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
+		fgets(buffer, BUFFER_LEN, fin);
 		while (fscanf(fin, "%d|", &tid) == 1) {
 			int k = 0; char c;
 			while ((c = (char)fgetc(fin)) != '|')
-				buf[k++] = c;
-			string tag_name(buf, k);
+				buffer[k++] = c;
+			string tag_name(buffer, k);
 			id_map[tid] = (int)Data::tag_name.size();
 #ifdef DEBUG
 			Data::real_tag_id.push_back(tid);
 #endif
 			Data::tag_name.push_back(tag_name);
 			Data::tagid[tag_name] = (int)Data::tag_name.size() - 1;
-			fgets(buf, MAX_LINE_LEN, fin);
+			fgets(buffer, BUFFER_LEN, fin);
 		}
 		Data::ntag = (int)Data::tag_name.size();
 		fclose(fin);
@@ -131,7 +189,7 @@ void read_tags_forums(const string & dir) {
 
 	{		// read person->tags
 		FILE* fin = safe_open(dir + "/person_hasInterest_tag.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
+		fgets(buffer, BUFFER_LEN, fin);
 		while (fscanf(fin, "%d|%d", &pid, &tid) == 2) {
 			Data::tags[pid].insert(id_map[tid]);
 		}
@@ -142,8 +200,13 @@ void read_tags_forums(const string & dir) {
 	Data::tag_forums.resize(Data::ntag);
 	{
 		FILE* fin = safe_open(dir + "/forum_hasMember_person.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
-		while (fscanf(fin, "%d|%d", &fid, &pid) == 2) {
+		READ_TILL_EOL();
+		while (true) {
+			READ_INT(fid);
+			if (buffer == buf_end) break;
+			READ_INT(pid);
+			READ_TILL_EOL();
+
 			unordered_map<int, Forum*>::const_iterator itr = forum_idmap.find(fid);
 			if (itr != forum_idmap.end()) {
 				itr->second->persons.push_back(PersonInForum(pid));
@@ -155,14 +218,16 @@ void read_tags_forums(const string & dir) {
 				forum->persons.push_back(PersonInForum(pid));
 				forum_idmap[fid] = forum;
 			}
-			fgets(buf, MAX_LINE_LEN, fin);
 		}
 		fclose(fin);
 	}
 	{
 		FILE* fin = safe_open(dir + "/forum_hasTag_tag.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
-		while (fscanf(fin, "%d|%d", &fid, &tid) == 2) {
+		READ_TILL_EOL();
+		while (true) {
+			READ_INT(fid);
+			if (buffer == buf_end) break;
+			READ_INT(tid);
 			m_assert(id_map.find(tid) != id_map.end());
 			if (forum_idmap.find(fid) == forum_idmap.end()) continue;		// forums with no person in
 			int c_tid = id_map[tid];
@@ -174,10 +239,10 @@ void read_tags_forums(const string & dir) {
 
 void read_org_places(const string& fname, const vector<int>& org_places) {
 	FILE* fin = safe_open(fname);
-	fgets(buf, MAX_LINE_LEN, fin);
+	fgets(buffer, BUFFER_LEN, fin);
 	int oid, pid;
 	while (fscanf(fin, "%d|%d", &pid, &oid) == 2) {
-		fgets(buf, MAX_LINE_LEN, fin);
+		fgets(buffer, BUFFER_LEN, fin);
 		m_assert(oid % 10 == 0);
 
 		Data::places[org_places[oid / 10]].persons.push_back(
@@ -190,15 +255,15 @@ void build_places_tree(const string& dir) {
 	int pid, max_pid = 0;
 	{
 		FILE* fin = safe_open(dir + "/place.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
+		fgets(buffer, BUFFER_LEN, fin);
 		while (fscanf(fin, "%d|", &pid) == 1) {
 			int k = 0; char c;
 			while ((c = (char)fgetc(fin)) != '|')
-				buf[k++] = c;
-			string place_name(buf, k);
+				buffer[k++] = c;
+			string place_name(buffer, k);
 			Data::placeid[place_name] = pid;
 			update_max(max_pid, pid);
-			fgets(buf, MAX_LINE_LEN, fin);
+			fgets(buffer, BUFFER_LEN, fin);
 		}
 		Data::places.resize(max_pid + 1);
 		fclose(fin);
@@ -206,7 +271,7 @@ void build_places_tree(const string& dir) {
 
 	{
 		FILE* fin = safe_open(dir + "/place_isPartOf_place.csv");
-		fgets(buf, MAX_LINE_LEN, fin);
+		fgets(buffer, BUFFER_LEN, fin);
 		int p1, p2;
 		while (fscanf(fin, "%d|%d", &p1, &p2) == 2) {
 			Data::places[p2].sub_places.push_back(&Data::places[p1]);
@@ -219,7 +284,7 @@ void read_places(const string& dir) {
 	build_places_tree(dir);
 
 	FILE* fin = safe_open(dir + "/person_isLocatedIn_place.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	fgets(buffer, BUFFER_LEN, fin);
 	int person, place;
 	while (fscanf(fin, "%d|%d", &person, &place) == 2) {
 		Data::places[place].persons.push_back(
@@ -229,7 +294,7 @@ void read_places(const string& dir) {
 
 	vector<int> org_places;
 	fin = safe_open(dir + "/organisation_isLocatedIn_place.csv");
-	fgets(buf, MAX_LINE_LEN, fin);
+	fgets(buffer, BUFFER_LEN, fin);
 	int pid, oid;
 	while (fscanf(fin, "%d|%d", &oid, &pid) == 2) {
 		m_assert(oid % 10 == 0);
@@ -251,9 +316,13 @@ void read_places(const string& dir) {
 }
 
 void read_data(const string& dir) {		// may need to be implemented synchronously
+	Timer timer;
 	read_person_file(dir);
 	read_person_knows_person(dir);
 	read_comments(dir);
+	PP(timer.get_time());
 	read_tags_forums(dir);
+	PP(timer.get_time());
 	read_places(dir);
+	PP(timer.get_time());
 }

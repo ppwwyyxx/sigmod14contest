9d28fbb52cc74cc9033008fe8b1955acec672df2
williamljb <williamm2006@126.com>
2014-03-03 19:36:11 +0800

    a brute-force version of problem 3

---
+++ src/query3_force.cc
@@ -0,0 +1,105 @@
+//File: query3.cpp
+//Date: Mon Mar 03 14:57:35 2014 +0800
+//Author: Junbang Liang <williamm2006@126.com>
+//Method:	Online. For each query, find the subset of persons included.
+//			And do bfs for each person, adding required pairs. Finally sort and output.
+//			Complexity: O(N^2 * ntags * logN)
+
+#include "query3.h"
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int get_common_tag(int p1, int p2)
+{
+	TagSet &t1 = Data::tags[p1], &t2 = Data::tags[p2];
+	int ret = 0;
+	TagSet::iterator it1 = t1.begin(), it2 = t2.begin();
+	while (it1 != t1.end() && it2 != t2.end())
+	{
+		if (*it1 == *it2)
+			++ret;
+		if (*it1 < *it2)
+			++it1;
+		else
+			++it2;
+	}
+	//printf("%d and %d : %d\n", p1, p2, ret);
+	return ret;
+}
+
+void Query3Handler::bfs(int pid, int h)
+{
+	vector<bool> vst(Data::nperson, false);
+	deque<int> q;
+	q.push_back(pid);
+	vst[pid] = true;
+	int depth = 0;
+	while (depth < h)
+	{
+		int now_size = q.size();
+		while (now_size--)
+		{
+			int cur_id = q.front();
+			q.pop_front();
+			vector<ConnectedPerson>& friends = Data::friends[cur_id];
+			for (vector<ConnectedPerson>::iterator it = friends.begin(); it != friends.end(); ++it)
+			{
+				int person = it->pid;
+				if (!vst[person])
+				{
+					q.push_back(person);
+					vst[person] = true;
+					if (pinplace.find(person) != pinplace.end() && person > pid)
+						answers.push_back(Answer3(get_common_tag(pid, person), pid, person));
+				}
+			}
+		}
+		depth++;
+	}
+}
+
+void Query3Handler::add_query(int k, int h, const string& p) {
+	//printf("\t\t\t%s\n", p.c_str());
+	//query input
+	queries.push_back(Query3(k, h, p));
+	//init
+	pset.clear();
+	answers.clear();
+	pinplace.clear();
+	//union sets
+	for (vector<int>::iterator it = Data::placeid[p].begin(); it != Data::placeid[p].end(); ++it)
+	{
+		PersonSet sub = Data::places[*it].get_all_persons();
+		PersonSet tmp; tmp.swap(pset);
+		pset.resize(tmp.size() + sub.size());
+		PersonSet::iterator pset_end = set_union(
+				sub.begin(), sub.end(),
+				tmp.begin(), tmp.end(), pset.begin());
+		pset.resize(std::distance(pset.begin(), pset_end));
+	}
+	//pickup people
+	for (PersonSet::iterator it = pset.begin(); it != pset.end(); ++it)
+	{
+		pinplace.insert(it->pid);
+		//printf("%d ", it->pid);
+	}
+	//printf("\n");
+	//bfs
+	for (PersonSet::iterator it = pset.begin(); it != pset.end(); ++it)
+		bfs(it->pid, h);
+	//sort and output
+	sort(answers.begin(), answers.begin() + answers.size());
+	for (vector<Answer3>::iterator it = answers.begin(); it != answers.end() && (k--); ++it)
+		printf("%d|%d ", it->p1, it->p2);
+	printf("\n");
+}
+
+void Query3Handler::work() {
+
+}
+
+void Query3Handler::print_result() {
+
+}

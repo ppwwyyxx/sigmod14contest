222788e6574fe756c742b172dc2da419019a2c8f
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-03-03 18:57:29 +0800

    fix memcpy problem

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 # File: run.sh
-# Date: Mon Mar 03 16:38:16 2014 +0800
+# Date: Mon Mar 03 18:35:42 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 [[ -z "$1" ]] && (echo "Usage: $0 /path/to/data/directory/ /path/to/query/file" && exit 1)
 
 #make -C src
-echo $2
 ./main "$1" "$2"

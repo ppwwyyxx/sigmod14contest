9f45b23507b680794d0947309f2ac18a131c4c62
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-03-11 11:56:20 +0800

    compile flags

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 # File: run.sh
-# Date: Mon Mar 10 17:36:06 2014 +0800
+# Date: Tue Mar 11 11:55:25 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 #[[ -z "$1" ]] && (echo "Usage: $0 /path/to/data/directory/ /path/to/query/file" && exit 1)
 #make -C src
-export LD_PRELOAD=src/third-party/libtcmalloc.so.4.1.2
+#export LD_PRELOAD=src/third-party/libtcmalloc.so.4.1.2
 ./main "$1" "$2" #2>/dev/null

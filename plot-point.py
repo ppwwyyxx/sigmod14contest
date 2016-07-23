379a55112d572ae86d7424bf766a960c1771a7c0
Yuxin Wu <ppwwyyxxc@gmail.com>
2016-07-23 00:17:29 -0700

    update plot-point.py

--- plot-point.py
+++ plot-point.py
@@ -5,7 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fontm
 import argparse, sys
+from collections import defaultdict
 from itertools import chain
+
+#from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('text', usetex=True)
 
 STDIN_FNAME = '-'
 
@@ -13,27 +18,30 @@ def get_args():
     description = "plot points into graph."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-i', '--input',
-            help = 'input data file, "-" for stdin, default stdin',
+            help='input data file, "-" for stdin, default stdin',
             default='-')
     parser.add_argument('-o', '--output',
-            help = 'output image', default = '')
+            help='output image', default='')
     parser.add_argument('--show',
-            help = 'show the figure after rendered',
-            action = 'store_true')
+            help='show the figure after rendered',
+            action='store_true')
     parser.add_argument('-c', '--column',
             help="describe each column in data, for example 'x,y,y'. \
             Default to 'y' for one column and 'x,y' for two columns. \
             Plot attributes can be appended after 'y', like 'ythick;cr' \
+            By default, assume all columns are ys. \
             ")
     parser.add_argument('-t', '--title',
-            help = 'title of the graph',
-            default = '')
+            help='title of the graph',
+            default='')
     parser.add_argument('--xlabel',
-            help = 'x label',
+            help='x label',
             default = 'x')
     parser.add_argument('--ylabel',
-            help = 'y label',
-            default = 'y')
+            help='y label',
+            default='y')
+    parser.add_argument('-s', '--scale',
+            help='scale of each y, separated by comma')
     parser.add_argument('--annotate-maximum',
             help = 'annonate maximum value in graph',
             action = 'store_true')
@@ -46,11 +54,10 @@ def get_args():
     parser.add_argument('--decay',
             help='exponential decay rate to smooth Y',
             type=float, default=0)
-    parser.add_argument('--legend',
+    parser.add_argument('-l', '--legend',
             help='legend for each y')
-    parser.add_argument('--ignore-difflen',
-            help='ignore the length different in each column, take the minimum',
-            action='store_true')
+    parser.add_argument('-d', '--delimeter',
+            help='column delimeter', default='\t')
 
     global args
     args = parser.parse_args();
@@ -143,25 +150,44 @@ def plot_args_from_column_desc(desc):
             ret['color'] = v[1:]
     return ret
 
-def do_plot(data_x, data_ys):
+def do_plot(data_xs, data_ys):
+    """
+    data_xs: list of 1d array, either of size 1 of size len(data_ys)
+    data_ys: list of 1d array
+    """
     fig = plt.figure(figsize = (16.18/1.2, 10/1.2))
     ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
-    nr_y = len(data_ys[0])
+    nr_y = len(data_ys)
     y_column = args.y_column
-    assert len(y_column) == nr_y
+
+    # parse legend and y-scale
     if args.legend:
         legends = args.legend.split(',')
         assert len(legends) == nr_y
     else:
-        legends = None
+        legends = None #range(nr_y) #None
+    if args.scale:
+        scale = map(float, args.scale.split(','))
+        assert len(scale) == nr_y
+    else:
+        scale = [1.0] * nr_y
+
     for yidx in range(nr_y):
         plotargs = plot_args_from_column_desc(y_column[yidx][1:])
-        data_y = data_ys[:,yidx]
+        now_scale = scale[yidx]
+        data_y = data_ys[yidx] * now_scale
         leg = legends[yidx] if legends else None
-        p = plt.plot(data_x, data_y, label=leg, **plotargs)
+        if now_scale != 1:
+            leg = "{}*{}".format(now_scale if int(now_scale) != now_scale else int(now_scale), leg)
+        data_x = data_xs[0] if len(data_xs) == 1 else data_xs[yidx]
+        assert len(data_x) >= len(data_y), \
+                "x column is shorter than y column! {} < {}".format(
+                        len(data_x), len(data_y))
+        truncate_data_x = data_x[:len(data_y)]
+        p = plt.plot(truncate_data_x, data_y, label=leg, **plotargs)
 
         c = p[0].get_color()
-        plt.fill_between(data_x, data_y, alpha=0.1, facecolor=c)
+        plt.fill_between(truncate_data_x, data_y, alpha=0.1, facecolor=c)
 
         #ax.set_aspect('equal', 'datalim')
         #ax.spines['right'].set_color('none')
@@ -170,11 +196,16 @@ def do_plot(data_x, data_ys):
         #plt.yticks([])
 
         if args.annotate_maximum or args.annotate_minimum:
-            annotate_min_max(data_x, data_y, ax)
+            annotate_min_max(truncate_data_x, data_y, ax)
 
     plt.xlabel(args.xlabel, fontsize='xx-large')
     plt.ylabel(args.ylabel, fontsize='xx-large')
     plt.legend(loc='best', fontsize='xx-large')
+
+    # adjust maxx
+    minx, maxx = min(data_x), max(data_x)
+    new_maxx = maxx + (maxx - minx) * 0.05
+    plt.xlim(minx, new_maxx)
 
     for label in chain.from_iterable(
             [ax.get_xticklabels(), ax.get_yticklabels()]):
@@ -191,6 +222,7 @@ def do_plot(data_x, data_ys):
 
 def main():
     get_args()
+    # parse input args
     if args.input == STDIN_FNAME:
         fin = sys.stdin
     else:
@@ -199,57 +231,66 @@ def main():
     if args.input != STDIN_FNAME:
         fin.close()
 
+    # parse column format
     nr_column = len(all_inputs[0].rstrip().split())
     if args.column is None:
-        if nr_column == 1:
-            column = ['y']
-        elif nr_column == 2:
-            column = ['x', 'y']
-        else:
-           raise Exception("Please specify column by '-c/--column'.")
+        column = ['y'] * nr_column
     else:
         column = args.column.strip().split(',')
     for k in column: assert k[0] in ['x', 'y']
     assert nr_column == len(column), "Column and data doesn't have same length. {}!={}".format(nr_column, len(column))
     args.y_column = [v for v in column if v[0] == 'y']
-    nr_x_column = column.count('x')
-    assert nr_x_column <= 1, "At most one 'x' is allowed"
+    args.y_column_idx = [idx for idx, v in enumerate(column) if v[0] == 'y']
+    args.x_column = [v for v in column if v[0] == 'x']
+    args.x_column_idx = [idx for idx, v in enumerate(column) if v[0] == 'x']
+    nr_x_column = len(args.x_column)
+    nr_y_column = len(args.y_column)
+    if nr_x_column > 1:
+        assert nr_x_column == nr_y_column, \
+            "If multiple x columns are used, nr_x_column must equals to nr_y_column"
 
-    data_x = []
-    data_ys = []
+    x_column_set = set(args.x_column)
+    # read and parse data
+    data = [[] for _ in range(nr_column)]
+    ended = defaultdict(bool)
     data_format = -1
     for lineno, line in enumerate(all_inputs):
-        line = [float(i) for i in line.rstrip().split()]
-        if not args.ignore_difflen:
-            assert len(line) == nr_column, \
-                    "Data doesn't have same length! Use --ignore-difflen to ignore."
-        else:
-            if len(line) != nr_column:
+        line = line.rstrip('\n').split(args.delimeter)
+        assert len(line) <= nr_column, \
+            """One row have too many columns (separated by {})!
+Line: {}""".format(repr(args.delimeter), line)
+        for idx, val in enumerate(line):
+            if val == '':
+                ended[idx] = True
                 continue
-
-        data_ys.append([])
-        for val, tp in zip(line, column):
-            if tp == 'x':
-                data_x.append(val)
             else:
-                data_ys[-1].append(val)
-        if nr_x_column == 0:
-            data_x.append(lineno + 1)
-    print "Data size: ", len(data_x)
-    assert len(data_x) == len(data_ys)
+                val = float(val)
+                assert not ended[idx], "Column {} has hole!".format(idx)
+                data[idx].append(val)
 
-    if len(data_x) == 1:
-        return
+    data_ys = [data[k] for k in args.y_column_idx]
+    max_ysize = max([len(t) for t in data_ys])
+    print "Size of the longest y column: ", max_ysize
 
-    data_ys = np.array(data_ys) # nr x dim
-    if args.decay != 0:
-        data_ys = exponential_smooth(data_ys, args.decay)
+    if nr_x_column:
+        data_xs = [data[k] for k in args.x_column_idx]
+    else:
+        data_xs = [list(range(max_ysize))]
+
+    for idx, data_y in enumerate(data_ys):
+        data_ys[idx] = np.asarray(data_y)
+        if args.decay != 0:
+            data_ys[idx] = exponential_smooth(data_y, args.decay)
+        #if idx == 0:   # TODO allow different decay for each y
+            #data_ys[idx] = exponential_smooth(data_y, 0.5)
+    for idx, data_x in enumerate(data_xs):
+        data_xs[idx] = np.asarray(data_x)
 
     if args.xkcd:
         with plt.xkcd():
-            do_plot(data_x, data_ys)
+            do_plot(data_xs, data_ys)
     else:
-        do_plot(data_x, data_ys)
+        do_plot(data_xs, data_ys)
 
 if __name__ == '__main__':
     main()
